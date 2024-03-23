f125d81163fa863d8605550d72d5fe3bfd6168ee
usman61 <usman.dcse@gmail.com>
2024-03-23 11:01:35 +0500

    send response to user

---
+++ send-resp.js
@@ -0,0 +1,14 @@
+// send response as html 
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url,req.method);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Demo</title></head>');
+    res.write('<body>My First NodeJS response</body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
